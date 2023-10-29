06048f55e65cac2b7d4fff581421894329bfc2d9
梦小天幼 <2090311442@qq.com>
2023-10-29 18:43:42 +0800

    使用Odin重写ToDo，并增加二级分类

--- Assets/Plugins/Todo/Editor/TodoList.cs
+++ Assets/Plugins/Todo/Editor/TodoList.cs
@@ -5,206 +5,212 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
-public class TodoList : EditorWindow
+namespace TODO 
 {
-	private static TodoList _window;
-	private TaskDataList taskSO;
-    private string _listDataDirectory = "/Plugins/Todo/";
-	private string _listDataAssetPath = "Assets/Plugins/Todo/TodoList.asset";
-	private int currentSelectLabelIndex = 0;
-	private int newTaskLabelIndex = 0;
-	private string newTask;
-	private bool showCompletedTasks = true;
-	private Vector2 scrollPosition = Vector2.zero;
-	private GUIStyle taskTextStyle;
-	private int displayCount = 0;
-	private GUILayoutOption heightOption;
-	[MenuItem ("Tool/Todo Window")]
-    public static void Init ()
-    {
-        _window = ( TodoList )EditorWindow.GetWindow (typeof ( TodoList ));
-		_window.titleContent = new GUIContent("ToDo");
-		// 当场景发生变化，是否重新绘制
-		_window.autoRepaintOnSceneChange = false;
-    }
-    
-	public void OnGUI ()
+	public class TodoList : EditorWindow
 	{
-		// 加载或创建任务列表资源
-		if (taskSO == null)
+		private static TodoList _window;
+		private TaskDataList taskSO;
+		private string _listDataDirectory = "/Plugins/Todo/";
+		private string _listDataAssetPath = "Assets/Plugins/Todo/TodoList.asset";
+		private int currentSelectLabelIndex = 0;
+		private int newTaskLabelIndex = 0;
+		private string newTask;
+		private bool showCompletedTasks = true;
+		private Vector2 scrollPosition = Vector2.zero;
+		private GUIStyle taskTextStyle;
+		private int displayCount = 0;
+		private GUILayoutOption heightOption;
+		[MenuItem("Tool/Todo Window")]
+		public static void Init()
 		{
-			taskSO = AssetDatabase.LoadAssetAtPath( _listDataAssetPath, typeof(TaskDataList)) as TaskDataList;
-			if(taskSO == null)
+			_window = (TodoList)EditorWindow.GetWindow(typeof(TodoList));
+			_window.titleContent = new GUIContent("ToDo");
+			// 当场景发生变化，是否重新绘制
+			_window.autoRepaintOnSceneChange = false;
+		}
+
+		public void OnGUI()
+		{
+			//Debug.Log("-------------------------");
+			//Debug.Log(_listDataAssetPath);
+			// 加载或创建任务列表资源
+			if (taskSO == null)
 			{
-				// 自动创建一个ToDo资源
-				taskSO = ScriptableObject.CreateInstance(typeof(TaskDataList)) as TaskDataList;
-                System.IO.Directory.CreateDirectory(Application.dataPath + _listDataDirectory);
-				AssetDatabase.CreateAsset(taskSO, _listDataAssetPath );
-				GUI.changed = true;				
-			}						
-		}
-
-		// +1 是因为下拉列表项需要有一个All Tasks
-		string[] labels = new string[taskSO.labels.Count + 1];
-		string[] labelsToSelect = new string[taskSO.labels.Count];
-		
-		labels[0] = "All Tasks";
-		for(int i = 0; i < taskSO.labels.Count; i++)
-		{
-			labels[i+1] = taskSO.labels[i].lableName;
-			labelsToSelect[i] = taskSO.labels[i].lableName;
-		}
-
-		// 使用水平布局，后续元素将横向排列
-		EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("小梦的待办列表:", EditorStyles.boldLabel);
-		// 创建一个下拉列表，index代表当前索引，labels代表需要下拉的项
-        currentSelectLabelIndex = EditorGUILayout.Popup(currentSelectLabelIndex, labels);
-		newTaskLabelIndex = currentSelectLabelIndex - 1;
-		EditorGUILayout.EndHorizontal();
-		
-
-		// 显示列表
-		taskTextStyle = new GUIStyle(EditorStyles.miniBoldLabel); // EditorStyles.wordWrappedMiniLabel 是 Unity 编辑器提供的一个内置样式，通常用于显示小型标签文本，允许文本自动换行。
-		taskTextStyle.alignment = TextAnchor.UpperLeft;
-		// taskTextStyle.wordWrap = true;
-
-		// 设置滚动视图
-		scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
-
-		// 显示 未完成 Task项
-		for( int i = 0; i < taskSO.tasks.Count; i++)
-		{
-			Task task = taskSO.tasks[i];
-			TaskLabel taskLabel = GetLabel(task.lableIndex);
-
-			//Debug.Log(currentSelectLabelIndex);
-			if(currentSelectLabelIndex == 0 && task.isComplete == false)
-			{	
-				CreateUnDoneTaskItem(task, i);
+				taskSO = AssetDatabase.LoadAssetAtPath(_listDataAssetPath, typeof(TaskDataList)) as TaskDataList;
+				if (taskSO == null)
+				{
+					// 自动创建一个ToDo资源
+					taskSO = ScriptableObject.CreateInstance(typeof(TaskDataList)) as TaskDataList;
+					System.IO.Directory.CreateDirectory(Application.dataPath + _listDataDirectory);
+					AssetDatabase.CreateAsset(taskSO, _listDataAssetPath);
+					GUI.changed = true;
+				}
 			}
-            else if(currentSelectLabelIndex > 0)
-            {
-                int adjustedIndex = currentSelectLabelIndex - 1;
-                taskLabel = taskSO.labels[adjustedIndex];
-                if (taskLabel.labelIndex == task.lableIndex && task.isComplete == false)
-                {
-                    CreateUnDoneTaskItem(task, i);
-                }
-            }
-        }
 
-        // 显示 已完成待办
-        for (int i = 0; i < taskSO.tasks.Count; i++)
-        {
-            Task task = taskSO.tasks[i];
-            TaskLabel taskLabel = GetLabel(task.lableIndex);
+			// +1 是因为下拉列表项需要有一个All Tasks
+			string[] labels = new string[taskSO.labels.Count + 1];
+			string[] labelsToSelect = new string[taskSO.labels.Count];
 
-			if (currentSelectLabelIndex == 0 && task.isComplete)
-            {
-                CreateDoneTaskItem(task, Color.gray, i);
-            }
-			else if(currentSelectLabelIndex > 0)
+			labels[0] = "All Tasks";
+			for (int i = 0; i < taskSO.labels.Count; i++)
 			{
-                int adjustedIndex = currentSelectLabelIndex - 1;
-                taskLabel = taskSO.labels[adjustedIndex];
-                if (taskLabel.labelIndex == task.lableIndex && task.isComplete)
-                {
-                    CreateDoneTaskItem(task, Color.gray, i);
-                }
-            }
-        }
+				labels[i + 1] = taskSO.labels[i].lableName;
+				labelsToSelect[i] = taskSO.labels[i].lableName;
+			}
 
-        if (displayCount == 0)
-        {
-			EditorGUILayout.LabelField("现在是摸鱼时间！~", EditorStyles.largeLabel);
-        }
-		EditorGUILayout.EndScrollView();
+			// 使用水平布局，后续元素将横向排列
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("小梦的待办列表:", EditorStyles.boldLabel);
+			// 创建一个下拉列表，index代表当前索引，labels代表需要下拉的项
+			currentSelectLabelIndex = EditorGUILayout.Popup(currentSelectLabelIndex, labels);
+			newTaskLabelIndex = currentSelectLabelIndex - 1;
+			EditorGUILayout.EndHorizontal();
 
-		// 创建任务
-		//EditorGUILayout.BeginHorizontal();
-		////EditorGUILayout.LabelField("Create Task:", EditorStyles.boldLabel);
-		//EditorGUILayout.EndHorizontal();
-		newTask = EditorGUILayout.TextField(newTask, GUILayout.Height(40));
-		if( ( GUILayout.Button("创建新待办") && newTask != "" ) && newTaskLabelIndex  >= 0)
-		{
-			TaskLabel newOwner = taskSO.labels[newTaskLabelIndex];
-			taskSO.AddTask(newTask, newOwner.labelIndex,GetDate());			
-			newTask = "";
-			GUI.FocusControl(null);				
+
+			// 显示列表
+			taskTextStyle = new GUIStyle(EditorStyles.miniBoldLabel); // EditorStyles.wordWrappedMiniLabel 是 Unity 编辑器提供的一个内置样式，通常用于显示小型标签文本，允许文本自动换行。
+			taskTextStyle.alignment = TextAnchor.UpperLeft;
+			// taskTextStyle.wordWrap = true;
+
+			// 设置滚动视图
+			scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+			// 显示 未完成 Task项
+			for (int i = 0; i < taskSO.tasks.Count; i++)
+			{
+				Task task = taskSO.tasks[i];
+				TaskLabel taskLabel = GetLabel(task.lableIndex);
+
+				//Debug.Log(currentSelectLabelIndex);
+				if (currentSelectLabelIndex == 0 && task.isComplete == false)
+				{
+					CreateUnDoneTaskItem(task, i);
+				}
+				else if (currentSelectLabelIndex > 0)
+				{
+					int adjustedIndex = currentSelectLabelIndex - 1;
+					taskLabel = taskSO.labels[adjustedIndex];
+					if (taskLabel.labelIndex == task.lableIndex && task.isComplete == false)
+					{
+						CreateUnDoneTaskItem(task, i);
+					}
+				}
+			}
+
+			// 显示 已完成待办
+			for (int i = 0; i < taskSO.tasks.Count; i++)
+			{
+				Task task = taskSO.tasks[i];
+				TaskLabel taskLabel = GetLabel(task.lableIndex);
+
+				if (currentSelectLabelIndex == 0 && task.isComplete)
+				{
+					CreateDoneTaskItem(task, Color.gray, i);
+				}
+				else if (currentSelectLabelIndex > 0)
+				{
+					int adjustedIndex = currentSelectLabelIndex - 1;
+					taskLabel = taskSO.labels[adjustedIndex];
+					if (taskLabel.labelIndex == task.lableIndex && task.isComplete)
+					{
+						CreateDoneTaskItem(task, Color.gray, i);
+					}
+				}
+			}
+
+			if (displayCount == 0)
+			{
+				EditorGUILayout.LabelField("现在是摸鱼时间！~", EditorStyles.largeLabel);
+			}
+			EditorGUILayout.EndScrollView();
+
+			// 创建任务
+			//EditorGUILayout.BeginHorizontal();
+			////EditorGUILayout.LabelField("Create Task:", EditorStyles.boldLabel);
+			//EditorGUILayout.EndHorizontal();
+			newTask = EditorGUILayout.TextField(newTask, GUILayout.Height(40));
+			if ((GUILayout.Button("创建新待办") && newTask != "") && newTaskLabelIndex >= 0)
+			{
+				TaskLabel newOwner = taskSO.labels[newTaskLabelIndex];
+				taskSO.AddTask(newTask, newOwner.labelIndex, GetDate());
+				newTask = "";
+				GUI.FocusControl(null);
+			}
+			if (GUI.changed)
+			{
+				EditorUtility.SetDirty(taskSO);
+				AssetDatabase.SaveAssets();
+			}
 		}
-		if (GUI.changed)
+
+		// 创建未完成待办显示项
+		public void CreateUnDoneTaskItem(Task currTask, int forI)
+		{
+			// UpdateLable();
+			taskTextStyle.normal.textColor = GetLabel(currTask.lableIndex).color;
+			displayCount++;
+			EditorGUILayout.BeginHorizontal();
+
+			if (EditorGUILayout.Toggle(currTask.isComplete, GUILayout.Width(15), GUILayout.Height(28)) == true)
+			{
+				taskSO.tasks[forI].isComplete = true;
+			}
+			taskSO.tasks[forI].taskName = EditorGUILayout.TextField(currTask.taskName, taskTextStyle);
+
+			EditorGUILayout.EndHorizontal();
+			EditorGUILayout.Space();
+		}
+
+		// 创建已完成待办显示项
+		public void CreateDoneTaskItem(Task currTask, Color color, int forI)
+		{
+			taskTextStyle.normal.textColor = color;
+			GUIStyle newStyle = taskTextStyle;
+			newStyle.alignment = TextAnchor.MiddleLeft;
+
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField(currTask.taskName, newStyle);
+			if (GUILayout.Button("X", GUILayout.Width(18), GUILayout.Height(18)))
+			{
+				taskSO.tasks.RemoveAt(forI);
+			}
+
+			EditorGUILayout.EndHorizontal();
+			EditorGUILayout.Space();
+		}
+
+		/// <summary>
+		/// 得到当前任务对应的标签
+		/// </summary>
+		/// <returns></returns>
+		public TaskLabel GetLabel(int index)
+		{
+			foreach (TaskLabel item in taskSO.labels)
+			{
+				if (item.labelIndex == index)
+					return item;
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// 得到当前日期
+		/// </summary>
+		/// <returns></returns>
+		public string GetDate()
+		{
+			DateTime currentDate = DateTime.Now;
+			string formattedDate = currentDate.ToString("yyyy-MM-dd");
+			return formattedDate;
+			//Debug.Log("Current Date: " + formattedDate);
+		}
+
+		void OnDestroy()
 		{
 			EditorUtility.SetDirty(taskSO);
-			AssetDatabase.SaveAssets();	
-		}	
-	}
-
-	// 创建未完成待办显示项
-	public void CreateUnDoneTaskItem(Task currTask,int forI)
-    {
-		// UpdateLable();
-		taskTextStyle.normal.textColor = GetLabel(currTask.lableIndex).color;
-		displayCount++;
-		EditorGUILayout.BeginHorizontal();
-
-		if (EditorGUILayout.Toggle(currTask.isComplete, GUILayout.Width(15), GUILayout.Height(28)) == true)
-		{
-			taskSO.tasks[forI].isComplete = true;
+			AssetDatabase.SaveAssets();
 		}
-		taskSO.tasks[forI].taskName = EditorGUILayout.TextField(currTask.taskName, taskTextStyle);
-
-		EditorGUILayout.EndHorizontal();
-		EditorGUILayout.Space();
-	}
-	
-	// 创建已完成待办显示项
-	public void CreateDoneTaskItem(Task currTask,Color color,int forI)
-    {
-		taskTextStyle.normal.textColor = color;
-		GUIStyle newStyle = taskTextStyle;
-		newStyle.alignment = TextAnchor.MiddleLeft;
-
-		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField(currTask.taskName, newStyle);
-		if (GUILayout.Button("X", GUILayout.Width(18), GUILayout.Height(18)))
-		{
-			taskSO.tasks.RemoveAt(forI);
-		}
-		
-		EditorGUILayout.EndHorizontal();
-		EditorGUILayout.Space();
 	}
 
-	/// <summary>
-	/// 得到当前任务对应的标签
-	/// </summary>
-	/// <returns></returns>
-	public TaskLabel GetLabel(int index)
-	{
-		foreach (TaskLabel item in taskSO.labels)
-		{
-			if (item.labelIndex == index)
-				return item;
-		}
-		return null;
-	}
-
-	/// <summary>
-	/// 得到当前日期
-	/// </summary>
-	/// <returns></returns>
-	public string GetDate()
-    {
-		DateTime currentDate = DateTime.Now; 
-		string formattedDate = currentDate.ToString("yyyy-MM-dd");
-		return formattedDate;
-		//Debug.Log("Current Date: " + formattedDate);
-	}
-
-	void OnDestroy()
-	{
-		EditorUtility.SetDirty(taskSO);
-		AssetDatabase.SaveAssets();
-	}	
 }
--- Assets/Plugins/Todo/SO/TaskDataList.cs
+++ Assets/Plugins/Todo/SO/TaskDataList.cs
@@ -4,102 +4,107 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-// 待做：按照日期排序
 
-public class TaskDataList : ScriptableObject
+namespace TODO
 {
-	/// <summary>
-	/// 标签列表
-	/// </summary>
-	public List<TaskLabel> labels = new List<TaskLabel>();
-	/// <summary>
-	/// 任务列表
-	/// </summary>
-	public List<Task> tasks = new List<Task>();
+	// 待做：按照日期排序
 
-	public ToDoSetting todoSetting;
-
-	public TaskDataList ()
-	{	
-		labels.Add( new TaskLabel("常规任务",0) );		
-		labels.Add( new TaskLabel("紧急任务", 1) );
-		labels.Add( new TaskLabel("正在完成", 2) );
-	}
-	
-	public void AddTask( string taskName, int lableIndex,string date)
+	public class TaskDataList : ScriptableObject
 	{
-		Task task = new Task( taskName, lableIndex, date);
-		tasks.Add(task);
-	}
-}
+		/// <summary>
+		/// 标签列表
+		/// </summary>
+		public List<TaskLabel> labels = new List<TaskLabel>();
+		/// <summary>
+		/// 任务列表
+		/// </summary>
+		public List<Task> tasks = new List<Task>();
 
-/// <summary>
-/// 任务
-/// </summary>
-[Serializable]
-public class Task
-{
+		public ToDoSetting todoSetting;
+
+		public TaskDataList()
+		{
+			labels.Add(new TaskLabel("常规任务", 0));
+			labels.Add(new TaskLabel("紧急任务", 1));
+			labels.Add(new TaskLabel("正在完成", 2));
+		}
+
+		public void AddTask(string taskName, int lableIndex, string date)
+		{
+			Task task = new Task(taskName, lableIndex, date);
+			tasks.Add(task);
+		}
+	}
+
 	/// <summary>
-	/// 任务名
+	/// 任务
 	/// </summary>
-	public string taskName;
-	/// <summary>
-	/// 任务所属标签序号
-	/// </summary>
-	public int lableIndex;
-	/// <summary>
-	/// 任务是否完成
-	/// </summary>
-	public bool isComplete;
-	/// <summary>
-	/// 创建日期
-	/// </summary>
-	public string creationDate;
-	
-	public Task( string task,int lableIndex ,string date)
+	[Serializable]
+	public class Task
 	{
-		this.taskName = task;
-		this.isComplete = false;
-		this.lableIndex = lableIndex;
-		creationDate = date;
+		/// <summary>
+		/// 任务名
+		/// </summary>
+		public string taskName;
+		/// <summary>
+		/// 任务所属标签序号
+		/// </summary>
+		public int lableIndex;
+		/// <summary>
+		/// 任务是否完成
+		/// </summary>
+		public bool isComplete;
+		/// <summary>
+		/// 创建日期
+		/// </summary>
+		public string creationDate;
+
+		public Task(string task, int lableIndex, string date)
+		{
+			this.taskName = task;
+			this.isComplete = false;
+			this.lableIndex = lableIndex;
+			creationDate = date;
+		}
+
+
 	}
 
-
-}
-
-/// <summary>
-/// 任务标签
-/// </summary>
-[Serializable]
-public class TaskLabel
-{
 	/// <summary>
-	/// 标签名
+	/// 任务标签
 	/// </summary>
-	public string lableName;
-	/// <summary>
-	/// 标签颜色
-	/// </summary>
-	public Color color;
-	/// <summary>
-	/// 标签序号
-	/// </summary>
-	public int labelIndex;
-	
-	public TaskLabel( string name, int index)
+	[Serializable]
+	public class TaskLabel
 	{
-		this.lableName = name;
-		this.color = Color.white;
-		this.labelIndex = index;
-	}
-}
+		/// <summary>
+		/// 标签名
+		/// </summary>
+		public string lableName;
+		/// <summary>
+		/// 标签颜色
+		/// </summary>
+		public Color color;
+		/// <summary>
+		/// 标签序号
+		/// </summary>
+		public int labelIndex;
 
-[Serializable]
-public class ToDoSetting
-{
-	/// <summary>
-	/// 是否自动生成（当找不到TODO文件的时候，防止Bug,文件被覆盖）
-	/// </summary>
-	public bool isAutoCreate;
+		public TaskLabel(string name, int index)
+		{
+			this.lableName = name;
+			this.color = Color.white;
+			this.labelIndex = index;
+		}
+	}
+
+	[Serializable]
+	public class ToDoSetting
+	{
+		/// <summary>
+		/// 是否自动生成（当找不到TODO文件的时候，防止Bug,文件被覆盖）
+		/// </summary>
+		public bool isAutoCreate;
+
+	}
 
 }
